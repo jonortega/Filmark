357169571d2edf27e56b762f6d97bda7da42a84d
jonortega <jon.ortega.g@gmail.com>
2023-06-12 13:52:01 +0200

    Created CustomInput widget

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../widgets/custom_input.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
@@ -11,49 +12,10 @@ class SearchScreenState extends State<SearchScreen> {
   String _displayText = '¡Hola, mundo!';
   final TextEditingController _textEditingController = TextEditingController();
 
-  @override
-  void dispose() {
-    _textEditingController.dispose();
-    super.dispose();
-  }
-
   void _changeText() {
     setState(() {
       _displayText = _textEditingController.text;
     });
-  }
-
-  // Decoracion del Input
-  InputDecoration _buildInputDecoration(String labelText, String hintText) {
-    return InputDecoration(
-      prefixIcon: const Icon(Icons.edit),
-      labelText: labelText,
-      labelStyle: const TextStyle(
-        color: Color.fromARGB(255, 161, 175, 161),
-        fontSize: 14,
-        fontWeight: FontWeight.normal,
-      ),
-      hintText: hintText,
-      hintStyle: const TextStyle(
-        color: Colors.grey,
-        fontStyle: FontStyle.italic,
-      ),
-      enabledBorder: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(6),
-        borderSide: const BorderSide(
-          color: Color.fromARGB(255, 161, 175, 161),
-          width: 2,
-        ),
-      ),
-      focusedBorder: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(6),
-        borderSide: const BorderSide(
-          color: Colors.green,
-          width: 2,
-        ),
-      ),
-      contentPadding: const EdgeInsets.symmetric(vertical: 14, horizontal: 18),
-    );
   }
 
   // Estilo del ElevatedButton
@@ -83,11 +45,7 @@ class SearchScreenState extends State<SearchScreen> {
           const SizedBox(height: 80),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 50),
-            child: TextField(
-              controller: _textEditingController,
-              decoration:
-                  _buildInputDecoration('Introduce el nuevo texto', 'Texto'),
-            ),
+            child: CustomImput(),
           ),
           const SizedBox(height: 20),
           ElevatedButton(
